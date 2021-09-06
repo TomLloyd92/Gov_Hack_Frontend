de48a5d82b07f87e80b2d11c8b732fbbf1a7b2ce
TomLloyd92 <c00231308@itcarlow.ie>
2021-09-06 15:42:10 +0100

    Time lock solidity contract

---
+++ gov_hack_frontend/ethereum/migrations/2_Full_migration.js
@@ -0,0 +1,41 @@
+const Faucet = artifacts.require("Faucet");
+const METAToken = artifacts.require("METAToken");
+const ErrorDecoder = artifacts.require("ErrorDecoder");
+const SafeMath = artifacts.require("SafeMath");
+const Timelock = artifacts.require("Timelock");
+
+const testAddress = '0x0687c9D588a481F8Fd546417b2FE94b50eA24102';
+const daysDelay = 0;
+
+module.exports = async function (deployer) {
+    //META TOKEN
+    await deployer.deploy(METAToken, testAddress);
+    const metaToken = await METAToken.deployed();
+    const metaTokenAddress = metaToken.address;
+
+    //FAUCET
+    await deployer.deploy(Faucet, metaTokenAddress);
+    const faucet = await Faucet.deployed();
+    const faucetAddress = faucet.address;
+
+    //ERROR DECODER
+    await deployer.deploy(ErrorDecoder);
+    const errorDecoder = await ErrorDecoder.deployed();
+    const errorDecoderAddress = errorDecoder.address;
+
+    //SafeMath
+    await deployer.deploy(SafeMath);
+    const safeMath = await SafeMath.deployed();
+    const safeMathAddress = safeMath.address;
+
+    //Time Lock
+    await deployer.deploy(Timelock, testAddress, daysDelay);
+    const timelock = await Timelock.deployed();
+    const timelockAddress = timelock.address;
+       
+    //console.log("ERROR DECODER ADDRESS: " + errorDecoderAddress);
+    //console.log("SAFE MATH ADDRESS: " + safeMathAddress);
+    console.log("META TOKEN ADDRESS: " + metaTokenAddress);
+    console.log("FAUCET ADDRESS: " + faucetAddress);
+    console.log("TIMELOCK ADDRESS: " + timelockAddress)
+};
